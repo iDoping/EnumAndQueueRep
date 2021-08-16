df0e1e753ffbce896c2e19f9f7d63f338fa64c25
iDoping <readovec@mail.ru>
2021-08-16 12:13:29 +0300

    Добавьте файлы проекта.

---
+++ LinqDec/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LinqDec
+{
+    static class MyExtensions
+    {
+        private class MyTakeEx<T> : IEnumerable<T>, IEnumerator<T>, IDisposable
+        {
+            private int _count;
+            private T _current;
+            private IEnumerable<T> _source;
+            private IEnumerator<T> _sourceE;
+
+            public MyTakeEx(IEnumerable<T> source, int count)
+            {
+                _count = count;
+                _source = source;
+                _sourceE = _source.GetEnumerator();
+            }
+
+            void IDisposable.Dispose()
+            {
+            }
+
+            bool IEnumerator.MoveNext()
+            {
+                if (_count != 0 && _sourceE.MoveNext())
+                {
+                        _count--;
+                        return true;                  
+                }
+                else return false;
+            }
+
+            public T Current => _sourceE.Current;
+            
+            void IEnumerator.Reset()
+            {
+                throw new NotSupportedException();
+            }
+
+            object IEnumerator.Current => _current;
+
+
+            IEnumerator<T> IEnumerable<T>.GetEnumerator()
+            {
+                return this;
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return _source.GetEnumerator();
+            }
+        }
+
+        private class MyWhereEx<T> : IEnumerable<T>, IEnumerable, IEnumerator<T>, IEnumerator, IDisposable
+        {
+            private Func<T, bool> _predicate;
+            private T _current;
+            private IEnumerable<T> _source;
+            private IEnumerator<T> _sourceE;
+
+            public MyWhereEx(Func<T, bool> predicate, IEnumerable<T> source)
+            {
+                _predicate = predicate;
+                _source = source;
+                _sourceE = _source.GetEnumerator();
+            }
+
+            public bool MoveNext()
+            {
+                while (_sourceE.MoveNext())
+                {
+                    var temp = _sourceE.Current; //убрать
+                    if (_predicate(temp))
+                    {
+                        _current = temp;
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            public T Current //по аналогии
+            {
+                get { return _current; }
+            }
+
+            object IEnumerator.Current
+            {
+                get { return _current; }
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public IEnumerator<T> GetEnumerator()
+            {
+                return this;
+            }
+
+            public void Reset()
+            {
+                throw new NotImplementedException();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return _source.GetEnumerator();
+            }
+        }
+
+        public static IEnumerable<T> MyWhere<T>(this IEnumerable<T> source, Func<T, bool> condition)
+        {
+            var temp = new MyWhereEx<T>(condition, source);
+            return temp;
+        }
+
+        public static IEnumerable<T> MyTake<T>(this IEnumerable<T> source, int count)
+        {
+            var temp = new MyTakeEx<T>(source, count);
+            return temp;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in GetEnumeration().MyTake(5).MyWhere(x => x % 2 == 0))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static IEnumerable<int> GetEnumeration()
+        {
+            int i = 0;
+            while (true)
+            {
+                yield return i++;
+            }
+        }
+    }
+}
