fa306e7095e541d664b7a847f0e06e2c1fa3a9b3
iDoping <readovec@mail.ru>
2021-08-27 16:35:44 +0300

    Initial commit

---
+++ EnumExtencions/EnumExtencions/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace EnumExtencions
+{
+    static class MyExtensions
+    {
+        private class MyTakeEx<T> : IEnumerable<T>, IEnumerator<T>, IDisposable
+        {
+            private int _count;
+            private T _current;
+            private IEnumerable<T> _source;
+            private IEnumerator<T> _sourceE;
+
+            public MyTakeEx(IEnumerable<T> source, int count)
+            {
+                _count = count;
+                _source = source;
+                _sourceE = _source.GetEnumerator();
+            }
+
+            void IDisposable.Dispose()
+            {
+            }
+
+            bool IEnumerator.MoveNext()
+            {
+                if (_count != 0 && _sourceE.MoveNext())
+                {
+                    _count--;
+                    return true;
+                }
+                else return false;
+            }
+
+            public T Current => _sourceE.Current;
+
+            object IEnumerator.Current => _current;
+
+            void IEnumerator.Reset()
+            {
+                throw new NotSupportedException();
+            }
+
+            IEnumerator<T> IEnumerable<T>.GetEnumerator()
+            {
+                return this;
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return _source.GetEnumerator();
+            }
+        }
+
+        private class MyWhereEx<T> : IEnumerable<T>, IEnumerator<T>, IDisposable
+        {
+            private Func<T, bool> _predicate;
+            private T _current;
+            private IEnumerable<T> _source;
+            private IEnumerator<T> _sourceE;
+
+            public MyWhereEx(Func<T, bool> predicate, IEnumerable<T> source)
+            {
+                _predicate = predicate;
+                _source = source;
+                _sourceE = _source.GetEnumerator();
+            }
+
+            public bool MoveNext()
+            {
+                while (_sourceE.MoveNext())
+                {
+                    if (_predicate(_sourceE.Current))
+                    {
+                        _current = _sourceE.Current;
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            public T Current => _sourceE.Current;
+
+            object IEnumerator.Current => _current;
+
+            public void Dispose()
+            {
+            }
+
+            public IEnumerator<T> GetEnumerator()
+            {
+                return this;
+            }
+
+            public void Reset()
+            {
+                throw new NotImplementedException();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return _source.GetEnumerator();
+            }
+        }
+
+        public static IEnumerable<T> MyWhere<T>(this IEnumerable<T> source, Func<T, bool> condition)
+        {
+            var temp = new MyWhereEx<T>(condition, source);
+            return temp;
+        }
+
+        public static IEnumerable<T> MyTake<T>(this IEnumerable<T> source, int count)
+        {
+            var temp = new MyTakeEx<T>(source, count);
+            return temp;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in GetEnumeration().MyTake(5).MyWhere(x => x % 2 == 0))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static IEnumerable<int> GetEnumeration()
+        {
+            int i = 0;
+            while (true)
+            {
+                yield return i++;
+            }
+        }
+    }
+}
---
+++ NodeQueue/NodeQueue/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace NodeQueue
+{
+    /// <summary>
+    /// Интерфейс IQueue
+    /// </summary>
+    public interface IQueue<T> : IEnumerable
+    {
+        public T Dequeue();
+        public void Enqueue(T data);
+        public int Count { get; }
+    }
+    /// <summary>
+    /// Класс, описывающий объект в очереди
+    /// </summary>
+    public class Node<T>
+    {
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; }
+        public Node<T> Next { get; set; }
+    }
+
+    /// <summary>
+    /// Класс, описывающий очередь
+    /// </summary>
+    public class MyQueue<T> : IQueue<T>
+    {
+        Node<T> head;
+        Node<T> tail;
+        private int _size;
+        private int _version = 0;
+
+        /// <summary>
+        /// Метод, с помощью которого происходит добавление элемента в очередь
+        /// <param name="data"> содержимое объекта, добавляемого в очередь
+        /// </summary>
+        public void Enqueue(T data)
+        {
+            if (data == null)
+                throw new InvalidOperationException("Object content is null");
+            Node<T> node = new Node<T>(data);
+            Node<T> tempNode = tail;
+            tail = node;
+            if (_size == 0)
+                head = tail;
+            else
+                tempNode.Next = tail;
+            _size++;
+            _version++;
+        }
+
+        /// <summary>
+        /// Метод, с помощью которого происходит извлечение элемента из очереди
+        /// <returns> Извлеченный объект из очереди </returns>
+        /// </summary>
+        public T Dequeue()
+        {
+            if (_size == 0)
+                throw new InvalidOperationException("There are no items in the queue");
+            T output = head.Data;
+            head = head.Next;
+            _size--;
+            _version++;
+            return output;
+        }
+
+        /// <summary>
+        /// Метод, возвращающий количество элементов в очереди       
+        /// <returns> Количество элементов в очереди </returns>
+        /// </summary>
+        public int Count { get { return _size; } }
+
+        /// <summary>
+        /// Реализация интерфейса IEnumerable
+        /// </summary>
+        public IEnumerator GetEnumerator()
+        {
+            return new QueueEnum<T>(this);
+        }
+
+        /// <summary>
+        /// Класс, реализующий итератор
+        /// </summary>
+        public class QueueEnum<T> : IEnumerator<T>
+        {
+            private bool firstMove = false;
+            private MyQueue<T> _queue;
+            private int _version;
+            private Node<T> _currentElement;
+            private bool disposed = false;
+            /// <summary>
+            /// Конструктор класса QueueEnum
+            /// </summary>
+            public QueueEnum(MyQueue<T> q)
+            {
+                _queue = q;
+                _version = _queue._version;
+                _currentElement = _queue.head;
+            }
+
+            /// <summary>
+            /// Обобщенная версия метода Current, с помощью которого происходит получение текущего элтемента в итераторе
+            /// </summary>
+            public T Current
+            {
+                get
+                {
+                    ThrowIfDisposed();
+                    return _currentElement.Data;
+                }
+            }
+
+            /// <summary>
+            /// Необобщенная версия метода Current, с помощью которого происходит получение текущего элтемента в итераторе
+            /// </summary>
+            object IEnumerator.Current => Current;
+
+            /// <summary>
+            /// Реализация интерфейса IDisposable.
+            /// </summary>
+            public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+
+            /// <summary>
+            /// Освобождение управляемых ресурсов
+            /// </summary>
+            protected virtual void Dispose(bool disposing)
+            {
+                if (!disposed)
+                {
+                    if (disposing)
+                    {
+                        _currentElement = null;
+                        _queue = null;
+                    }
+                    disposed = true;
+                }
+            }
+
+            private void ThrowIfDisposed()
+            {
+                if (disposed)
+                    throw new ObjectDisposedException("Object Disposed");
+            }
+
+            /// <summary>
+            /// Метод, с помощью которого происходит переход к следующему элементу
+            /// <returns> Возвращает true, если есть следующий элемент, false, если следующего элемента не существует </returns>
+            /// </summary>
+            public bool MoveNext()
+            {
+                ThrowIfDisposed();
+                if (_version != _queue._version)
+                    throw new InvalidOperationException("Unable to change queue while loop is running");
+                if (_currentElement.Next != null && firstMove == true)
+                    _currentElement = _currentElement.Next;
+                else if (firstMove == false)
+                {
+                    firstMove = true;
+                }
+                else
+                    return false;
+                return true;
+            }
+
+            /// <summary>
+            /// Метод, устанавливающий итератор в начальное положение
+            /// </summary>
+            public void Reset()
+            {
+                ThrowIfDisposed();
+                if (_version != _queue._version)
+                    throw new InvalidOperationException("Unable to change queue while loop is running");
+                _currentElement = _queue.head;
+                firstMove = false;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
